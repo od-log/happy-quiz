4ae9a9b37e358db8d70f32fdb5988efa087dee6f
OD <kg-te@naver.com>
2022-12-05 18:23:47 +0900

    ✨ 오답노트 컴포넌트 추가

--- src/pages/ImproveNote/index.tsx
+++ src/pages/ImproveNote/index.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { CardLayout } from "@src/components";
+import { Answer } from "@src/types/storage";
+import { useNavigate } from "react-router-dom";
 
 const ImproveNote = () => {
-  return <div>오답 노트</div>;
+  const navigate = useNavigate();
+  let answerObj = localStorage.getItem("answerObj");
+  let newAnswerObj: Answer = {};
+  if (answerObj) newAnswerObj = JSON.parse(answerObj);
+
+  let wrongQuizList = Object.keys(newAnswerObj).filter(
+    (item) => newAnswerObj[item]?.mark === "false"
+  );
+
+  return (
+    <CardLayout>
+      <h2>오답 노트</h2>
+      <ul>
+        {wrongQuizList &&
+          wrongQuizList.map((item, index) => {
+            return (
+              <li
+                onClick={() =>
+                  navigate(`/quiz/${Number(item)}`, { state: "note" })
+                }
+              >
+                {Number(item) + 1}번 문제 확인하기
+              </li>
+            );
+          })}
+      </ul>
+    </CardLayout>
+  );
 };
 
 export default ImproveNote;
